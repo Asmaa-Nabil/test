776b32e459d1a73460b812a997c2b52f26bc2696
anabilmss <asmaa26697@gmail.com>
2022-05-12 21:28:09 +0200

    Asmaa

---
+++ js/email.js
@@ -0,0 +1,15 @@
+function cutEmail(){
+    var emailInput = document.getElementById("email");
+    var nameInput = document.getElementById("name");
+    var domainInput = document.getElementById("domain");
+
+    if(emailInput.value.includes("@")){
+        var indexOfAt = emailInput.value.indexOf("@");
+        var nameOfEmail = emailInput.value.slice(0, indexOfAt);
+        nameInput.value = nameOfEmail;
+        domainInput.value = emailInput.value.slice(indexOfAt+1);
+    }
+    else{
+        alert("Please input a corrct email")
+    }
+}
